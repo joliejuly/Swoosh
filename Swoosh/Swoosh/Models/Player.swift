03384c8d2a2ae6536992290149519195c2c5e762
Julia Nikitina <julianikitina.ios@gmail.com>
2018-08-23 23:06:20 +0300

    Add Player model

---
+++ Swoosh/Swoosh/Models/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  Swoosh
+//
+//  Created by Julia Nikitina on 23/08/2018.
+//  Copyright © 2018 Julia Nikitina. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String? 
+}

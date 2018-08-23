39b729d34ce5f15ffd6a3f80c359c025ea987769
Julia Nikitina <julianikitina.ios@gmail.com>
2018-08-23 18:28:51 +0300

    Add league button choice

--- Swoosh/Swoosh/DesiredLeagueViewController.swift
+++ Swoosh/Swoosh/DesiredLeagueViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class DesiredLeagueViewController: UIViewController {
-
+    
+    @IBOutlet var leagueButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.isHidden = false
@@ -18,5 +20,17 @@ class DesiredLeagueViewController: UIViewController {
     override var prefersStatusBarHidden: Bool {
         return false
     }
-
+    
+    
+    @IBAction func leagueIsChosen(_ sender: UIButton) {
+        sender.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.5)
+        for (index, button) in leagueButtons.enumerated() {
+            if sender.tag != index {
+                button.backgroundColor = .clear
+            }
+        }
+        
+        
+    }
+    
 }
